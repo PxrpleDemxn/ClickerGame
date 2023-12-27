51d81096c4ef1bceb6bafaac486477b028834b70
PxrpleDemxn <malca9616@gmail.com>
2023-12-27 14:07:36 +0100

    GameStats
    Created game stats so we can get stats everywhere

---
+++ Scripts/IServices/IGameStatsService.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+public partial interface IGameStatsService
+{
+	/// <summary>
+	/// Adds coins
+	/// </summary>
+	/// <param name="coins">Coins</param>
+	void AddCoins(double coins);
+
+	/// <summary>
+	/// Removes coins
+	/// </summary>
+	/// <param name="coins">Coins</param>
+	void RemoveCoins(double coins);
+
+	/// <summary>
+	/// Sets coins
+	/// </summary>
+	/// <param name="coins">Coins</param>
+	void SetCoins(double coins);
+
+	/// <summary>
+	/// Gets coins
+	/// </summary>
+	/// <returns>Coins</returns>
+	double GetCoins();
+
+	/// <summary>
+	/// Sets calculated income
+	/// </summary>
+	/// <param name="income">Income</param>
+	void SetIncome(double income);
+
+	/// <summary>
+	/// Gets calculated income
+	/// </summary>
+	/// <returns>Income</returns>
+	double GetIncome();
+
+	/// <summary>
+	/// Sets Game Stats
+	/// </summary>
+	/// <param name="gameStats">Game Stats</param>
+	void SetGameStats(GameStats gameStats);
+
+	/// <summary>
+	/// Gets GameStats
+	/// </summary>
+	/// <returns>GameStats</returns>
+	GameStats GetGameStats();
+}
---
+++ Scripts/Services/GameStatsService.cs
@@ -0,0 +1,66 @@
+using Godot;
+using System;
+
+public partial class GameStatsService : IGameStatsService
+{
+	private static GameStatsService _instance;
+	private GameStats _gameStats;
+
+	private GameStatsService()
+	{
+		_gameStats = new();
+	}
+
+	public static GameStatsService Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = new GameStatsService();
+			}
+			return _instance;
+		}
+	}
+
+	public void AddCoins(double coins)
+	{
+		_gameStats.Coins += coins;
+	}
+
+	public void RemoveCoins(double coins)
+	{
+		_gameStats.Coins -= coins;
+	}
+
+	public double GetCoins()
+	{
+		return _gameStats.Coins;
+	}
+
+	public double GetIncome()
+	{
+		return _gameStats.Income;
+	}
+
+	public void SetCoins(double coins)
+	{
+		_gameStats.Coins = coins;
+	}
+
+	public void SetIncome(double income)
+	{
+		_gameStats.Income = income;
+	}
+
+	public void SetGameStats(GameStats gameStats)
+	{
+		_gameStats = gameStats;
+	}
+
+	public GameStats GetGameStats()
+	{
+		return _gameStats;
+	}
+
+}
