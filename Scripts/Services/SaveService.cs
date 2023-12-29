67823c9d9610b9b5248878848f24df0c9594ae90
MagicNightFox <50232151+MagicNightFox@users.noreply.github.com>
2023-12-29 21:29:12 +0100

    Load a Save Enemy listu

--- Scripts/Services/SaveService.cs
+++ Scripts/Services/SaveService.cs
@@ -7,8 +7,12 @@ public partial class SaveService : ISaveService
 {
   IUpgradeService _upgradeService = UpgradeService.Instance;
   IGameStatsService _gameStats = GameStatsService.Instance;
+
+  IEnemyService _enemy = EnemyService.Instance;
   const string path = "res://test.json"; // path, change it later!!
   const string savePath = "res://save000.json"; // savePath, change it later!!
+
+  const string enemySavePath = "res://enemies.json"; // enemySavePath, change it later!!
   public void SaveGame()
   {
     // TODO
@@ -66,4 +70,30 @@ public partial class SaveService : ISaveService
     // Using Newtonsoft.Json we deserialize object to automatically convert it back to GameStats
     _gameStats.SetGameStats(JsonConvert.DeserializeObject<GameStats>(load.GetLine()));
   }
+
+  public void SaveEnemy(Enemy enemy)
+  {
+    // Opens path and sets flag to write
+    using var save = FileAccess.Open(enemySavePath, FileAccess.ModeFlags.Write);
+    
+    // Using Newtonsoft.Json we serialize object to automatically convert it to json
+    string json = JsonConvert.SerializeObject(_enemy.GetEnemies());
+
+    save.StoreLine(json);
+
+  }
+
+  public void LoadEnemy(){
+    // Opens path and sets flag to read
+    using var load = FileAccess.Open(enemySavePath, FileAccess.ModeFlags.Read);
+    // Checks if load file exists... if not it create new empty List<Enemy>
+    if (load == null)
+    {
+      _enemy.SetEnemies(new List<Enemy>());
+      return;
+    }
+    // Using Newtonsoft.Json we deserialize object to automatically convert it back to List<Enemy>
+    _enemy.SetEnemies(JsonConvert.DeserializeObject<List<Enemy>>(load.GetLine()));
+  }
+
 }
